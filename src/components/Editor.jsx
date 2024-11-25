e6ea11d118bb25dbeae5ea1c4973949f5800377e
wjinss <aastu4343@gmail.com>
2024-11-25 15:15:41 +0900

    Editor 기능 구현

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,8 @@
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 import Button from "./Button";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const emotionList = [
   {
@@ -25,33 +27,100 @@ const emotionList = [
   },
 ];
 
-const Editor = () => {
-  const emotionId = 1;
+//input type="date"는 new Date()를 벨류값으로 못받아서 문자열로 받아야됨
+const getStringedDate = (targetDate) => {
+  //날짜 > YYYY-MM-DD 형태로 만들어야됨
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (date < 10) {
+    date = `0${date}`;
+  }
+  return `${year}-${month}-${date}`;
+};
+
+const Editor = ({ onSubmit }) => {
+  const nav = useNavigate();
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: "",
+  });
+
+  // 브라우저에서 날짜를 변경시켜 현재 createdDate라는 "input"을 수정하면 변경된 입력값이 문자열로 반환됨
+  const onChangeInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    // 반환된 문자열을 데이트 객체로 바꾸는 함수
+    if (name === "createdDate") {
+      // 오늘의 날짜를 수정했다 > 문자열로 반환되면~
+      value = new Date(value);
+      //벨류값을 데이트 객체로 변환해서 값을 설정
+    }
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+  const onClickSubmitButton = () => {
+    onSubmit(input);
+  };
+
   return (
     <div className="Editor">
       <section className="date_section">
         <h4>오늘의 날짜</h4>
-        <input type="date" />
+        <input
+          type="date"
+          name="createdDate"
+          value={getStringedDate(input.createdDate)}
+          onChange={onChangeInput}
+        />
       </section>
       <section className="emotion_section">
         <h4>오늘의 감정</h4>
         <div className="emotion_list_wrapper">
           {emotionList.map((item) => (
             <EmotionItem
+              onClick={() =>
+                onChangeInput({
+                  target: {
+                    name: "emotionId",
+                    value: item.emotionId,
+                  },
+                })
+              }
               key={item.emotionId}
               {...item}
-              isSelected={item.emotionId === emotionId}
+              isSelected={item.emotionId === input.emotionId}
             />
           ))}
         </div>
       </section>
       <section className="content_section">
         <h4>오늘의 일기</h4>
-        <textarea placeholder="오늘은 어땠나요?" />
+        <textarea
+          placeholder="오늘은 어땠나요?"
+          name="content"
+          value={input.content}
+          onChange={onChangeInput}
+        />
       </section>
       <section className="button_section">
-        <Button text={"취소하기"} />
-        <Button text={"작성완료"} type={"POSITIVE"} />
+        <Button
+          text={"취소하기"}
+          onClick={() => {
+            nav(-1);
+          }}
+        />
+        <Button
+          text={"작성완료"}
+          type={"POSITIVE"}
+          onClick={onClickSubmitButton}
+        />
       </section>
     </div>
   );
--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,14 +1,25 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 const New = () => {
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true }); //replace: true 뒤로가기 방지
+  };
+
   return (
     <div>
       <Header
         title={"새 일기 쓰기"}
-        leftChild={<Button text={`< 뒤로 가기`} />}
+        leftChild={<Button text={`< 뒤로 가기`} onClick={() => nav(-1)} />} // 네비게이션 함수에 -1 입력시 뒤로가기가 됨
       />
-      <Editor />
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
